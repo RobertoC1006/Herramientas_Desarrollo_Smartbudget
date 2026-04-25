f5ba54a267164c4e8efd58446b255a4daa899d83
Fabian Abad <fabianabadmija@gmail.com>
2026-04-25 11:49:48 -0500

    Eliminacion archivos

--- core/enums.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-class ExpenseCategoryEnum(str, Enum):
-    COMIDA = "comida"
-    TRANSPORTE = "transporte"
-    SERVICIOS = "servicios"
-    OCIO = "ocio"
-    OTROS = "otros"
